6a1cce7a4b288f4378a9df7ba42dab3d983e3dcc
siwon1105 <77540999+siwown1105@users.noreply.github.com>
2025-07-08 00:11:51 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ self.addEventListener('install', event => {
         './index.html',
         './manifest.json',
         './icon-192.png',
-        './icon-512.png'
+        
       ]);
     })
   );
